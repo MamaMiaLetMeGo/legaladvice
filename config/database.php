cffcc98f52b61b0d20caf3df8c3763395d8d4d6a
Charles Gendron <charlesgendron@Charless-MacBook-Air.local>
2024-11-21 06:43:54 -0500

    updated database.php

--- config/database.php
+++ config/database.php
@@ -94,7 +94,10 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'require',
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
+            'options' => [
+                PDO::ATTR_PERSISTENT => false,
+                PDO::ATTR_TIMEOUT => 60,
         ],
 
         'sqlsrv' => [
