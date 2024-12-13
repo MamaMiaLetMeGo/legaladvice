36d76e1a38ee808559ef7953214c0c5b17fc1985
Charles Gendron <charlesgendron@Charless-MacBook-Air.local>
2024-12-12 23:45:16 -0300

    undid what we did to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,17 +8,4 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    build: {
-        rollupOptions: {
-            external: ['alpinejs'],
-            output: {
-                globals: {
-                    alpinejs: 'Alpine'
-                }
-            }
-        }
-    },
-    optimizeDeps: {
-        exclude: ['alpinejs']
-    }
 });
